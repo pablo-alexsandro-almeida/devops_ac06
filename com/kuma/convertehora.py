9c49dc7c6dd197e41cc774bf412ea63af69ea1bc
pablo-alexsandro-almeida <pablo.almeida@aluno.faculdadeimpacta.com.br>
2019-09-27 20:23:00 -0300

    commit 2

--- com/kuma/convertehora.py
+++ com/kuma/convertehora.py
@@ -1,13 +1,16 @@
 def converteHora(hora24, minuto24):
-    if (hora24 > 23) or (hora24 < 0) or (minuto24 < 0) or (minuto24 > 59):
-        return None
+	'''
+	Esta funão tem como objetivo de converter horas 
+	'''
+    if hora24 > 23 or hora24 < 0 or minuto24 < 0 or minuto24 > 59:
+	return None
 
-    if (hora24 < 12):
-        if (hora24 == 0):
+    if hora24 < 12:
+        if hora24 == 0:
             hora24 = 12
-        return '%02d:%02d AM' % (hora24, minuto24)
+			return '%02d:%02d AM' % (hora24, minuto24)
 
-    if (hora24 > 12):
+    if hora24 > 12:
         hora24 -= 12
     return '%02d:%02d PM' % (hora24, minuto24)
 	
