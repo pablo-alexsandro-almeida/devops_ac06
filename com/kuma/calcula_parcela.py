16b70472bac640f2b8b24cba45be9d95afb8e9d8
pablo-alexsandro-almeida <53957344+pablo-alexsandro-almeida@users.noreply.github.com>
2019-09-27 21:04:36 -0300

    Update calcula_parcela.py

--- com/kuma/calcula_parcela.py
+++ com/kuma/calcula_parcela.py
@@ -1,19 +1,24 @@
-def valorPagamento(valor, diasAtraso):
+def valorpagamento(valor, diasatraso):
+    '''
+    função para calcular valor do pagamento
+    '''
 
     if (valor < 0):
         return None
-    if (diasAtraso > 0):
+    if (diasatraso > 0):
         multa = valor * 0.03
-        adicionalAtraso = valor * (diasAtraso * 0.01)
-        return valor + multa + adicionalAtraso
+        adicionalatraso = valor * (diasatraso * 0.01)
+        return valor + multa + adicionalatraso
     else:
         return valor
+
+
 '''
 # Entrada de Dados
 valor = 1
 while (valor != 0):
     valor = float(raw_input('Informe o valor da prestacao: '))
     if (valor != 0):
-        diasAtraso = int(raw_input('Informe a quantidade de dias de atraso: '))
-        print ("Valor a ser pago: %.2f" % valorPagamento(valor, diasAtraso))
+        diasatraso = int(raw_input('Informe a quantidade de dias de atraso: '))
+        print ("Valor a ser pago: %.2f" % valorPagamento(valor, diasatraso))
 '''
