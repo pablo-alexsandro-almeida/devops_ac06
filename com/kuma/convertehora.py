b2163e265f86544c6e751150ead9fb1f8f178a12
pablo-alexsandro-almeida <53957344+pablo-alexsandro-almeida@users.noreply.github.com>
2019-09-27 21:03:10 -0300

    Update convertehora.py

--- com/kuma/convertehora.py
+++ com/kuma/convertehora.py
@@ -1,24 +1,27 @@
-def valorpagamento(valor, diasatraso):
-    '''
-    funçao para calcular o valor de converte
-    '''
+def converteHora(hora24, minuto24):
+	'''
+	Esta funão tem como objetivo de converter horas 
+	'''
+    if hora24 > 23 or hora24 < 0 or minuto24 < 0 or minuto24 > 59:
+		return None
 
-    if valor < 0:
-        return None
-    if diasatraso > 0:
-        multa = valor * 0.03
-        adicionalatraso = valor * (diasatraso * 0.01)
-        return valor + multa + adicionalatraso
-    else:
-        return valor
+    if hora24 < 12:
+        if hora24 == 0:
+            hora24 = 12
+			return '%02d:%02d AM' % (hora24, minuto24)
 
-
+    if hora24 > 12:
+        hora24 -= 12
+    return '%02d:%02d PM' % (hora24, minuto24)
+	
 '''
-# Entrada de Dados
-valor = 1
-while (valor != 0):
-    valor = float(raw_input('Informe o valor da prestacao: '))
-    if (valor != 0):
-        diasatraso = int(raw_input('Informe a quantidade de dias de atraso: '))
-        print ("Valor a ser pago: %.2f" % valorPagamento(valor, diasatraso))
+# Entrada de dados
+continuar = 'S'
+while (continuar == 'S'):
+    hora = int(raw_input('Informe o valor da hora: '))
+    minuto = int(raw_input('Informe o valor dos minutos: '))
+
+    print converteHora(hora, minuto)
+
+    continuar = raw_input('Deseja continuar (S/N): ').upper()
 '''
