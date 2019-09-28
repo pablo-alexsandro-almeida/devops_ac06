2cd964f1c6a3fe62b3288b3cc7b1fa91bcca8144
pablo-alexsandro-almeida <53957344+pablo-alexsandro-almeida@users.noreply.github.com>
2019-09-27 22:03:57 -0300

    Update calcula_parcela.py

--- com/kuma/calcula_parcela.py
+++ com/kuma/calcula_parcela.py
@@ -1,13 +1,13 @@
-def valorPagamento(valor, diasAtraso):
+def valor_pagamento(valor, dias_atraso):
     '''
     teste
     '''
     if valor < 0:
         return None
-    if diasAtraso > 0:
+    if dias_atraso > 0:
         multa = valor * 0.03
-        adicionalAtraso = valor * (diasAtraso * 0.01)
-        return valor + multa + adicionalAtraso
+        adicional_atraso = valor * (dias_atraso * 0.01)
+        return valor + multa + adicional_atraso
     else:
         return valor
 
