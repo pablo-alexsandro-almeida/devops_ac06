ba7cf163ace378a0b803983679310192ec550b27
pablo-alexsandro-almeida <53957344+pablo-alexsandro-almeida@users.noreply.github.com>
2019-09-27 21:51:49 -0300

    Update test_operacoes.py

--- com/kuma/test/test_operacoes.py
+++ com/kuma/test/test_operacoes.py
@@ -1,57 +1,87 @@
 import pytest
-import valorpagamento from com.kuma.calcula_parcela 
-import contacorrente from com.kuma.contaCorrente 
-import convertehora from com.kuma.convertehora
+from com.kuma.calcula_parcela import valorPagamento
+from com.kuma.contaCorrente import ContaCorrente
+from com.kuma.convertehora import converteHora
 
 
 def test_deposito():
+    '''
+    teste 
+    '''
     contacorrente = contacorrente(1222, "pablo")
     contacorrente.deposito(100)
     assert contacorrente.saldo == 100, "Valor e 100"
 
 
 def test_alterarnome():
+    '''
+    teste 
+    '''
     contacorrente = contacorrente(1222, "pablo")
     contacorrente.alterar_Nome("julio")
     assert contacorrente.nomeCorrentista == "julio", " Era Pablo"
 
 
 def test_saque():
+    '''
+    teste 
+    '''
     contacorrente = contacorrente(1222, "pablo")
     contacorrente.saque(100)
     assert contacorrente.saldo == -100, "Valor e 100"
 
 
 def test_retornarnone():
+    '''
+    teste 
+    '''
     operacao = convertehora(24, 0)
     assert operacao == None, "Deveria ser None"
 
 
 def test_retornarmeiodia():
+    '''
+    teste 
+    '''
     operacao = convertehora(0, 10)
     assert operacao == "12:10 AM", "Deveria ser 12:10 AM"
 
 
 def test_retornaronze():
+    '''
+    teste 
+    '''
     operacao = convertehora(9, 10)
     assert operacao == "09:10 AM", "Deveria ser 9:10 AM"
 
 
 def test_retornarnone():
+    '''
+    teste 
+    '''
     operacao = convertehora(13, 10)
     assert operacao == "01:10 PM", "Deveria ser 1:10 AM"
 
 
 def test_doisdiasatraso():
+    '''
+    teste 
+    '''
     operacao = valorpagamento(100, 2)
     assert operacao == 105, "Deveria ser 105"
 
 
 def test_valorzero():
+    '''
+    teste 
+    '''
     operacao = valorpagamento(-1, 0)
     assert operacao == None, "Deveria ser 0"
 
 
 def test_sematraso():
+    '''
+    teste 
+    '''
     operacao = valorpagamento(100, 0)
     assert operacao == 100, "Deveria ser 100"
